c3a1fca1ed5d30bc4cb922fc0ecbd4028f75cd5c
No Face <86858788+0xNoFace@users.noreply.github.com>
2024-03-08 21:26:45 +0000

    the boots stay on

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Pigeon In Yellow Boots</title>
+        <meta name="Pigeon In Yellow Boots" content="The boots stay on" />
+        <link rel="shortcut icon" href="favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
